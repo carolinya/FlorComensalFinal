017515473853440a829bf895202e8414b39674b5
hernja <hernja666@gmail.com>
2018-07-17 17:32:58 -0500

    update

--- Altas/conexion.php
+++ Altas/conexion.php
@@ -1,11 +1,7 @@
 <?php
-$host     = 'localhost:8080';
+$host     = 'localhost';
 $user     = 'root';
-<<<<<<< HEAD
-$password = 'tania1189';
-=======
 $password = '';
->>>>>>> 76c1421551f224c1cb04a39392ae4a1f95fd2b9a
 $db       = 'comensal';
 
 $conection = @mysqli_connect($host, $user, $password, $db);
